03305774c5aabe01e043f24b993127e22cdc6557
pjavadi84 <pouya.javadi.resume@gmail.com>
2020-03-06 13:08:58 -0800

    Done.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -12,18 +12,52 @@ class PostsController < ApplicationController
 	end
 
 	def create
-	  @post = Post.new(params["post"])
-	  @post.save
-	  redirect_to post_path(@post)
+	# WITHOUT USING STRONG PARAMS SECURITY FEATURE
+	#   @post = Post.new(params["post"])
+	#   @post.save
+	#   redirect_to post_path(@post)
+
+	#WITH STRONG PARAMS FEATURE. WITH THIS, NO ONE CAN ALTER OUR PARAMETERS
+	#   @post = Post.new(params.require(:post).permit(:title, :description))
+  	#   @post.save
+	#   redirect_to post_path(@post)
+
+	#REFACTORING STRONG PARAMS USING POST_PARAMS PRIVATE METHOD
+		@post = Post.new(post_params(:title, :description))
+		@post.save
+		redirect_to post_path(@post)
 	end
 
 	def update
-	  @post = Post.find(params[:id])
-	  @post.update(params["post"])
-	  redirect_to post_path(@post)
+		# WITHOUT USING STRONG PARAMS SECURITY FEATURE
+		#   @post = Post.find(params[:id])
+		#   @post.update(params["post"])
+		#   redirect_to post_path(@post)
+
+		#WITH STRONG PARAMS FEATURE. WITH THIS, NO ONE CAN ALTER OUR PARAMETERS
+		# @post = Post.find(params[:id])
+		# @post.update(params.require(:post).permit(:title))
+		# redirect_to post_path(@post)
+
+		#REFACTORING STRONG PARAMS USING POST_PARAMS PRIVATE METHOD
+			@post = Post.find(params[:id])
+			@post.update(post_params(:title))
+			redirect_to post_path(@post)
 	end
 
 	def edit
 	  @post = Post.find(params[:id])
 	end
+
+	private
+	#pre-refactored post_params method
+	# def post_params
+	# 	params.require(:post).permit(:title, :description)
+	# end
+	
+	# refactored post_params methods with *args as argument
+	def post_params(*args)
+		params.require(:post).permit(*args)
+	end
+
 end
